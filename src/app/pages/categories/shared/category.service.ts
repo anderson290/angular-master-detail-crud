bbcff314ac97dbd22bffa0c70fb0d72f49897574
unknown <anderson.nunes17@etec.sp.gov.br>
2020-12-26 00:43:24 -0300

    improving category list component

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -24,7 +24,6 @@ export class CategoryService {
 
   getById(id: number): Observable<Category> {
     const url = `${this.apiPath}/${id}`;
-
     return this.http.get(url).pipe(
       catchError(this.handleError),
       map(this.jsonDataToCategory)
