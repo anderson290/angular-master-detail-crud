caf02ad284bfec4c89c5cc15190516bd95619d06
unknown <anderson.nunes17@etec.sp.gov.br>
2020-12-25 23:57:19 -0300

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,14 +1,15 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Category } from "./pages/categories/shared/category.model";
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             {id: 1, name: "Moradia", description: "Pagamentos de contas da casa"},
             {id: 2, name: "Saude", description: "Plano de Saude e Remedios"},
             {id: 3, name: "Lazer", description: "Cinema, Parques, etc"},
             {id: 4, name: "Salario", description: "Recebimento de salario"},
             {id: 5, name: "Freelas", description: "Trabalhos com freelancer"}
-        ]
+        ];
         return { categories };
     }
 }
