2a44d15653d7c58605da5a761bdaea2b79d38430
unknown <anderson.nunes17@etec.sp.gov.br>
2020-12-28 23:07:10 -0300

    Creating breadcrumb | adding routermodule into sharedModule | improving componentes with breadcrumbComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreadCrumbComponent
+  ],
   imports: [
     CommonModule,
+    RouterModule,
     ReactiveFormsModule
   ],
   exports: [
+    //shared components
+    BreadCrumbComponent,
+    //shared modules
     CommonModule,
+    RouterModule,
     ReactiveFormsModule
   ]
 })
